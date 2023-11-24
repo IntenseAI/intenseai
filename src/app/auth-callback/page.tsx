75a17ba3332d277f4ddefe26cbc340b86054f410
AKSAATHWIK <151855972+IntenseAI@users.noreply.github.com>
2023-11-24 18:59:35 +0530

    update

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,42 +1,54 @@
-"use client"
+"use client";
 
-import { useRouter, useSearchParams } from 'next/navigation'
-import { trpc } from '../_trpc/client'
-import { Loader2 } from 'lucide-react'
+import { useRouter, useSearchParams } from "next/navigation";
+import { trpc } from "../_trpc/client";
+import { Loader2 } from "lucide-react";
+import { useRef, useState } from "react";
 
 const Page = () => {
-  const router = useRouter()
+  const router = useRouter();
+  const retry = useRef(0);
+  const maxRetryCount = 5;
+  const searchParams = useSearchParams();
+  const origin = searchParams.get("origin");
 
-  const searchParams = useSearchParams()
-  const origin = searchParams.get('origin')
-
-  trpc.authCallback.useQuery(undefined, {
+  const { refetch } = trpc.authCallback.useQuery(undefined, {
     onSuccess: ({ success }) => {
       if (success) {
         // user is synced to db
-        router.push(origin ? `/${origin}` : '/dashboard')
+        router.push(origin ? `/${origin}` : "/dashboard");
       }
     },
     onError: (err) => {
-      if (err.data?.code === 'UNAUTHORIZED') {
-        router.push('/sign-in')
+      console.log(err);
+      if (err.data?.code === "UNAUTHORIZED") {
+        retry.current = retry.current + 1;
+        if (retry.current <= maxRetryCount) {
+          // Retry up to maxRetryCount
+          setTimeout(() => {
+            refetch();
+          }, 500);
+        } else {
+          router.push("/sign-in");
+        }
       }
     },
-    retry: true,
+
+    retry: false,
     retryDelay: 500,
-  })
+  });
 
   return (
-    <div className='w-full mt-24 flex justify-center'>
-      <div className='flex flex-col items-center gap-2'>
-        <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
-        <h3 className='font-semibold text-xl'>
-          Setting up your account...
+    <div className="w-full mt-24 flex justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+        <h3 className="font-semibold text-xl">
+          We are setting up your account
         </h3>
-        <p>You will be redirected automatically.</p>
+        <p>You will be redirected, Automatically</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
